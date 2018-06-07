530a19166cbf381cfa411c8ca37e7c2ec559eee2
huog <1102194607@qq.com>
2018-06-07 16:03:16 +0800

    添加右下角信息展示组件

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -2,16 +2,55 @@
   <div class="win10-message" :style="{right: right}">
     <div class="win10-message-header">
       消息中心
-      <span>全部清除</span>
+      <span @click="closeAllMsg">全部清除</span>
+    </div>
+    <div class="win10-message-content">
+      <MessageList :messageList="messageList"></MessageList>
     </div>
   </div>
 </template>
 
 <script>
+let MessageList = () => import('./MessageList.vue');
+
 export default {
+  components: {
+    MessageList
+  },
+  data () {
+    return {
+      messageList: [
+        {
+          title: 'IT运维统计报表界面',
+          text: 'IT运维统计报表界面，点击会话id为409的查询按钮，在会话回显界面，exit命令的显示结果中有乱码显示；',
+          uuid: '00001'
+        },
+        {
+          title: 'IT运维统计报表界面',
+          text: 'IT运维统计报表界面，点击会话id为409的查询按钮，在会话回显界面，exit命令的显示结果中有乱码显示；',
+          uuid: '00002'
+        },
+        {
+          title: 'fa',
+          text: 'faknk',
+          uuid: '00003'
+        },
+        {
+          title: 'fa',
+          text: 'faknk',
+          uuid: '00004'
+        }
+      ]
+    };
+  },
   computed: {
     right () {
       return this.$store.state.showMessage ? '0' : '-360px';
+    }
+  },
+  methods: {
+    closeAllMsg () {
+      this.messageList = [];
     }
   }
 };
@@ -50,5 +89,10 @@ export default {
 .win10-message-header span:hover {
   color: #fff;
   cursor: pointer;
+}
+.win10-message-content {
+  font-weight: normal;
+  color: #666666;
+  line-height: normal;
 }
 </style>
