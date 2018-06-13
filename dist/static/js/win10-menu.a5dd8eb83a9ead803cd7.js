4e1611d9437756f2684d117c54d1ce0c2b37234c
xhlf <zhengxin0913@vip.qq.com>
2018-06-13 11:07:04 +0800

    shortcut

--- dist/static/js/win10-menu.a5dd8eb83a9ead803cd7.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{"6ivd":function(t,e){},ccZh:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var s={render:function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"win10-menu",style:{bottom:this.bottom}},[this._v(" menu")])},staticRenderFns:[]};var i=n("VU/8")({computed:{bottom:function(){return this.$store.state.showMenu?"0":"-480px"}}},s,!1,function(t){n("6ivd")},null,null);e.default=i.exports}});
-//# sourceMappingURL=win10-menu.a5dd8eb83a9ead803cd7.js.map
