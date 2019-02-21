df9e6704ad580a9cc3b7b0f03ba23fd5f8571867
xhlf <zhengxin0913@vip.qq.com>
2019-02-21 16:08:28 +0800

    local gitpage

--- config/index.js
+++ config/index.js
@@ -50,7 +50,6 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
     assetsPublicPath: '/',  // 本地
     // assetsPublicPath: './', // github
 
